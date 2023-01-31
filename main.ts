86f062958d84cca6ab7b5f84af55cda9d5a6df07
ocantonio <122565417+ocantonio@users.noreply.github.com>
2023-01-31 14:23:58 +0100

    Actualizar pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Neron.vy = -150
+})
+let Neron: Sprite = null
+scene.setBackgroundColor(13)
+Neron = sprites.create(assets.image`Neron`, SpriteKind.Player)
+controller.moveSprite(Neron, 100, 0)
+tiles.setCurrentTilemap(tilemap`nivel1`)
+Neron.ay = 300
+scene.cameraFollowSprite(Neron)
+Neron.setPosition(8, 64)
