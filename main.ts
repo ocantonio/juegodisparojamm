4da8a94b263be97be8a78fcda7e53095fc37afa6
ocantonio <122565417+ocantonio@users.noreply.github.com>
2023-01-31 20:15:44 +0100

    Actualizar main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+namespace SpriteKind {
+    export const lStart = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`lsart`, function (sprite, location) {
+    sprites.destroy(sprite, effects.starField, 500)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd5, function (sprite, location) {
     info.changeLifeBy(-1)
     Neron.setPosition(8, 64)
@@ -5,8 +11,25 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd5, function (sprite,
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Neron.vy = -150
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    bala = sprites.createProjectileFromSprite(assets.image`proyectil`, Neron, 200, 0)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`lfinish`, function (sprite, location) {
+    game.gameOver(true)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    sprites.destroy(sprite)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    sprites.destroy(otherSprite)
+})
+let enemigo: Sprite = null
+let bala: Sprite = null
 let Neron: Sprite = null
-scene.setBackgroundColor(13)
+scene.setBackgroundColor(9)
 Neron = sprites.create(assets.image`Neron`, SpriteKind.Player)
 controller.moveSprite(Neron, 100, 0)
 tiles.setCurrentTilemap(tilemap`nivel1`)
@@ -14,5 +37,9 @@ Neron.ay = 300
 scene.cameraFollowSprite(Neron)
 Neron.setPosition(8, 64)
 info.setLife(3)
-let enemigo = sprites.create(assets.image`bola`, SpriteKind.Enemy)
-enemigo.setPosition(164, randint(0, 120))
+info.setScore(0)
+game.onUpdateInterval(1000, function () {
+    enemigo = sprites.create(assets.image`bola`, SpriteKind.Enemy)
+    enemigo.setVelocity(-80, 0)
+    enemigo.setPosition(164, randint(0, 120))
+})
