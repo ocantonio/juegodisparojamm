89a916e74533b04dfba868ca69c95cef9af85a4d
ocantonio <122565417+ocantonio@users.noreply.github.com>
2023-01-31 14:36:19 +0100

    Actualizar main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd5, function (sprite, location) {
+    info.changeLifeBy(-1)
+    Neron.setPosition(8, 64)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Neron.vy = -150
 })
@@ -9,3 +13,6 @@ tiles.setCurrentTilemap(tilemap`nivel1`)
 Neron.ay = 300
 scene.cameraFollowSprite(Neron)
 Neron.setPosition(8, 64)
+info.setLife(3)
+let enemigo = sprites.create(assets.image`bola`, SpriteKind.Enemy)
+enemigo.setPosition(164, randint(0, 120))
